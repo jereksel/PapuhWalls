1ba93c3943a2b81fdb55db1c163fca6c0e1a5911
jereksel <jereksel@gmail.com>
2015-10-01 17:39:49 +0200

    Next and last part

--- app/src/main/java/com/alexcruz/papuhwalls/Walls/OriginalWalls.java
+++ app/src/main/java/com/alexcruz/papuhwalls/Walls/OriginalWalls.java
@@ -1,31 +1,6 @@
 package com.alexcruz.papuhwalls.Walls;
 
-import android.content.Context;
-import android.content.Intent;
-
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
-
-import com.alexcruz.papuhwalls.JSONParser;
 import com.alexcruz.papuhwalls.R;
-import com.alexcruz.papuhwalls.WallsFragment;
-import com.alexcruz.papuhwalls.WallsGridAdapter;
-import com.github.mrengineer13.snackbar.SnackBar;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class OriginalWalls extends AbsWalls {
 
